ce5c27d70a2fc4033334fe2964d68466aaa5de6e
Panagiotis Garefalakis <pangaref@gmail.com>
2015-12-14 11:00:35 +0000

    Adding new infrastructure type for Seep multithread - the int version is number 2

--- seep-master/src/main/java/uk/ac/imperial/lsds/seepmaster/infrastructure/master/InfrastructureType.java
+++ seep-master/src/main/java/uk/ac/imperial/lsds/seepmaster/infrastructure/master/InfrastructureType.java
@@ -2,10 +2,11 @@ package uk.ac.imperial.lsds.seepmaster.infrastructure.master;
 
 public enum InfrastructureType {
 	PHYSICAL_CLUSTER(0), 
-	YARN_CLUSTER(1), 
-	DOCKER_CLUSTER(2), 
-	VIRTUAL_CLUSTER(3), 
-	SHARED_PHYSICAL_CLUSTER(4);
+	YARN_CLUSTER(1),
+	MULTITHREAD(2),
+	DOCKER_CLUSTER(3), 
+	VIRTUAL_CLUSTER(4), 
+	SHARED_PHYSICAL_CLUSTER(5);
 	
 	private int type;
 	
